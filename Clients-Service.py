5656314c06593a97a9ffca16d3a3753a38db1e0c
White_Hat_Hacker <83054516+SUHAYB-MACHINELEARNING-FULL-STACK@users.noreply.github.com>
2021-09-03 13:54:39 +0300

    Update Clients-Service.py

--- Clients-Service.py
+++ Clients-Service.py
@@ -1,1 +1,7 @@
+Data=open('CustomerData.txt','a')
 print("Enter 1 for SignUp as Customer\nEnter 2 for LogIn as Customer\nEnter 3 for LogIn as Admin")
+def SignUpAsCustomer(Email,Password):
+def LogInAsCustomer(Email,Password):
+def LogInAsAdmin(Email,Password):
+def GiveMeYourData():
+GiveMeYourData()
