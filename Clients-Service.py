49b3a48324bdc42c813f6fb46b728ce410d822b9
White_Hat_Hacker <83054516+SUHAYB-MACHINELEARNING-FULL-STACK@users.noreply.github.com>
2021-09-03 14:00:27 +0300

    Update Clients-Service.py

--- Clients-Service.py
+++ Clients-Service.py
@@ -1,5 +1,5 @@
 DataFile=open('CustomersData.txt','a')
-AdminDataDictionary={"Email":'',"Password":''}
+AdminDataDictionary={"Email":'Admin97334@lucidya.com',"Password":'040670c71a2ddf1f23d5559c15faeb190b12f836a0d2f1dbd0e362797df07acd'}
 CustomersDataDictionary={"Admin"}
 print("Enter 1 for SignUp as Customer\nEnter 2 for LogIn as Customer\nEnter 3 for LogIn as Admin")
 def SignUpAsCustomer(Email,Password):
